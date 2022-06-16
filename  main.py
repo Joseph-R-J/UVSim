446f6e66f441dc272fddad3af4c8342c4a8efbec
Joseph Jackson <JosephRJCS@gmail.com>
2022-06-16 04:04:38 -0600

    main.py created. Need to decide where GetUserInput will live. I suggest to do it in main, and then have the program load the information.  However If we want to have it exist in UvSim, it needs to be modified so as to be accessible from main.

---
+++  main.py
@@ -0,0 +1,16 @@
+def main(): 
+
+    # User Instructions
+    print("*** Welcome to UVSim! ***",
+    "*** Please enter your program one instruction ***",
+    "*** ( or data word ) at a time into the input ***",
+    "*** text field. I will display the location   ***",
+    "*** number and question mark (?). You then    ***",
+    "*** type the word for that location. Enter    ***",
+    "*** -99999 to stop entering your program.     ***",
+    sep='\n', end='\n')
+
+    
+
+if __name__ == "__main__":
+    main()
