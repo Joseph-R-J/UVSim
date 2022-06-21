4eb543b24c961399b41a392f9f4ebf54de7111dd
Jake <Jake@LAPTOP-35M8LVS6>
2022-06-21 17:40:00 -0600

    first commit

---
+++ src/Accumulator.py
@@ -0,0 +1,16 @@
+# Accumulator register class. Single variable for the accumulator, a get and set for that variable
+
+class Accumulator:
+    
+    def __init__(self, accumVal = 0): # start with zero
+        self._accumVal = accumVal
+
+    # getter 
+    def getAccum(self):
+        return self._accumVal
+
+    # setter 
+    def setAccum(self, x):
+        self._accumVal = x
+
+    pass
