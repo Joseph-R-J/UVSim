7bfb465866d1befc8ed4d8022248e38552a064d1
Joseph Jackson <JosephRJCS@gmail.com>
2022-06-22 09:23:38 -0600

    do load should be working now

--- src/LoadStoreOperations.py
+++ src/LoadStoreOperations.py
@@ -1,11 +1,14 @@
-# import MemoryRegister
 
 class LoadStoreOperations:
 
     # Load a word from a specific location in memory into the accumulator.
-    def load(MemoryRegisterArray, index):
-            return MemoryRegisterArray.getItemFromMemoryRegister(index)
-    
+    def load(MemoryRegisterArray, accumulator, index):
+        memoryItem = MemoryRegisterArray.getItemFromMemoryRegister(int(index))
+        accumulator.load(memoryItem)
+        return None
+
+
+
     # Store a word from the accumulator into a specific location in memory.
     def store(MemoryRegisterArray, item, index):
         MemoryRegisterArray.insertIntoMemoryRegister(item, index)
