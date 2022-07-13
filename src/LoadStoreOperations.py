ffa97b83c97606b26f1b421c054aace2619aa1bb
Mason Stringfellow <mason.blair.stringfellow@gmail.com>
2022-07-12 19:54:25 -0600

    added "self" to class methods

--- src/LoadStoreOperations.py
+++ src/LoadStoreOperations.py
@@ -2,14 +2,14 @@
 class LoadStoreOperations:
 
     # Load a word from a specific location in memory into the accumulator.
-    def load(MemoryRegisterArray, accumulator, index):
+    def load(self, MemoryRegisterArray, accumulator, index):
         memoryItem = MemoryRegisterArray.getItemFromMemoryRegister(index)
         accumulator.setAccum(memoryItem)
         return None
 
 
     # Store a word from the accumulator into a specific location in memory.
-    def store(MemoryRegisterArray, accumulator, index):
+    def store(self, MemoryRegisterArray, accumulator, index):
         accumItem = accumulator.getAccum()
         MemoryRegisterArray.insertIntoMemoryRegister(accumItem, index)
         return None
